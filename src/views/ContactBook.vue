0ac4f1be327015fb9da9c40f9a8e48ffaf27d589
duyduy0312 <duyb2203496@student.ctu.edu.vn>
2025-03-08 21:42:19 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -33,6 +33,16 @@
           <i class="fas fa-address-card"></i>
         </h4>
         <ContactCard :contact="activeContact" />
+        <router-link
+          :to="{
+            name: 'contact.edit',
+            params: { id: activeContact._id },
+          }"
+        >
+          <span class="mt-2 badge badge-warning">
+            <i class="fas fa-edit"></i> Hiệu chỉnh</span
+          >
+        </router-link>
       </div>
     </div>
   </div>
@@ -50,72 +60,72 @@ export default {
     ContactList,
   },
   data() {
-      return {
-          contacts: [],
-          activeIndex: -1,
-          searchText: "",
-      };
+    return {
+      contacts: [],
+      activeIndex: -1,
+      searchText: "",
+    };
   },
   watch: {
-      // Giám sát các thay đổi của biến searchText.
-      // Bỏ chọn phần tử đang được chọn trong danh sách.
-      searchText() {
+    // Giám sát các thay đổi của biến searchText.
+    // Bỏ chọn phần tử đang được chọn trong danh sách.
+    searchText() {
       this.activeIndex = -1;
-      },
+    },
   },
   computed: {
-      // Chuyển các đối tượng contact thành chuỗi để tiện cho tìm kiếm.
-      contactStrings() {
+    // Chuyển các đối tượng contact thành chuỗi để tiện cho tìm kiếm.
+    contactStrings() {
       return this.contacts.map((contact) => {
-          const { name, email, address, phone } = contact;
-      return [name, email, address, phone].join("");
+        const { name, email, address, phone } = contact;
+        return [name, email, address, phone].join("");
       });
-  },
+    },
     // Trả về các contact có chứa thông tin cần tìm kiếm.
     filteredContacts() {
-        if (!this.searchText) return this.contacts;
-        return this.contacts.filter((_contact, index) =>
-            this.contactStrings[index].includes(this.searchText)
-        );
-        },
+      if (!this.searchText) return this.contacts;
+      return this.contacts.filter((_contact, index) =>
+        this.contactStrings[index].includes(this.searchText)
+      );
+    },
     activeContact() {
-        if (this.activeIndex < 0) return null;
-        return this.filteredContacts[this.activeIndex];
-        },
+      if (this.activeIndex < 0) return null;
+      return this.filteredContacts[this.activeIndex];
+    },
     filteredContactsCount() {
-        return this.filteredContacts.length;
-        },
+      return this.filteredContacts.length;
     },
-    methods: {
-        async retrieveContacts() {
-            try {
-            this.contacts = await ContactService.getAll();
-            } catch (error) {
-            console.log(error);
-            }
-        },
+  },
+  methods: {
+    async retrieveContacts() {
+      try {
+        this.contacts = await ContactService.getAll();
+      } catch (error) {
+        console.log(error);
+      }
+    },
     refreshList() {
-        this.retrieveContacts();
-        this.activeIndex = -1;
-        },
+      this.retrieveContacts();
+      this.activeIndex = -1;
+    },
     async removeAllContacts() {
-        if (confirm("Bạn muốn xóa tất cả Liên hệ?")) {
-            try {
-                await ContactService.deleteAll();
-                this.refreshList();
-            } catch (error) {
-                console.log(error);
-              }
-            }
-        },
+      if (confirm("Bạn muốn xóa tất cả Liên hệ?")) {
+        try {
+          await ContactService.deleteAll();
+          this.refreshList();
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    },
     goToAddContact() {
-        this.$router.push({ name: "contact.add" });
-        },
+      this.$router.push({ name: "contact.add" });
     },
-    mounted() {
+  },
+  mounted() {
     this.refreshList();
-    },
-};  
+  },
+};
 </script>
 <style scoped>
 .page {
